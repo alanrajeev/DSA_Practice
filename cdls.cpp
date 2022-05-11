26699306229e20fa651569f2cd41986a2b3ecb91
alanrajeev <alanrajeev2001@gmail.com>
2022-05-11 08:52:49 +0530

    a

---
+++ cdls.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector <int> A(5,9);
+    for (int i = 0; i < A.size(); i++){
+        cout<<A[i];
+    } 
+    cout<<endl;
+    return 0;
+}
