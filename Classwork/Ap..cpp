a71ac81bd49fa0d6c4f1d8a08f007b96cf23d3a3
alanrajeev <alanrajeev2001@gmail.com>
2022-09-28 06:30:07 +0530

    pandas

--- Classwork/Ap..cpp
+++ Classwork/Ap..cpp
@@ -1,11 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a,n,d;
-    cin>>a>>n>>d;
-    for(int i=0;i<n;i++){
-        cout<<a<<" ";
-        a = a+d;
-    }   
+int main()
+{
+    string s = "My name is Alan.Alan is hardworking guy",d = "";
+    string sub = "Alan";
+    int n = s.length();
+    for(int i = 0;i<n;i++){
+        if(s[i]!= " "){
+            d += s[i];
+        }
+        else{
+            if(d==sub) {
+                cout<<true;
+                break;
+            }
+            d = "";
+        }
+    }
+    cout<<false;
+    return 0;
 }
